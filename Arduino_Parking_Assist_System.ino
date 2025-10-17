c6e93696fb21adc5758edc41b851d63078e7eedf
kamise09 <freewhale09@gmail.com>
2025-10-17 10:20:16 +0900

    create, buz, led

--- Arduino_Parking_Assist_System.ino
+++ Arduino_Parking_Assist_System.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+const pinLED = 7;
+const pinBuz = 8;
+int md = 0;
 
+void setup() {
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinBuz, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  if(md == 0){
+    digitalWrite(pinLED, HIGH);
+    tone(pinBuz, 1000);
+    delay(1000);
+    digitalWrite(pinLED, LOW);
+    md = 1;
+  }
 
 }

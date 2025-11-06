47d1f7b1afc6a1aba903212e7bc61302684756d8
kamise09 <freewhale09@gmail.com>
2025-11-07 00:59:11 +0900

    all function, variable ready

--- Arduino_Parking_Assist_System.ino
+++ Arduino_Parking_Assist_System.ino
@@ -1,14 +1,19 @@
 #include<math.h>
 
-const int pinLED = 7;
-const int pinBuz = 8;
-const int pinIR = A0;
-const int IN1 = 9;
+// 변수에 핀번호 할당
+const int pinLED = 7; // LED핀
+const int pinBuz = 8; // 부저 핀
+const int pinIR = A0; // 거리센서1
+const int pinIR2 = A1;// 거리센서2
+
+// 모터 드라이버 관련 핀 
+const int IN1 = 9; 
 const int IN2 = 10;
 const int IN3 = 11;
 const int IN4 = 12;
 
-const int8_t seq[8][4] = {
+
+const int8_t seq[8][4] = { // 스텝 구동을 위한 배열 
 	{1, 0, 0, 0},
 	{1, 1, 0, 0},
 	{0, 1, 0, 0},
@@ -19,13 +24,57 @@ const int8_t seq[8][4] = {
 	{1, 0, 0, 1}
 };
 
-// 한 바퀴 스텝 수 (half-step 기준). 필요하면 2048 ↔ 4096 사이에서 보정.
 long STEPS_PER_REV = 2048;
-
-// 스텝 간 지연(us). 값이 작을수록 빠르게 회전. 너무 작으면 토크 부족/스텝로스 가능.
 unsigned int step_delay_us = 1200;
 
-static int seqIndex = 0; // 시퀀스 위치 기억
+// 필요 함수 정의
+void stepOnce(int dir);
+void stepN(long steps, int dir);
+void rotateDegrees(float angle_deg);
+void disableCoils();
+void warning();
+double mmPrint(int x);
+
+
+void setup() {
+
+	// pinMode 초기화
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinBuz, OUTPUT);
+  pinMode(pinIR, INPUT);
+	pinMode(pinIR2, INPUT);
+  pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(IN3, OUTPUT);
+	pinMode(IN4, OUTPUT);
+
+	disableCoils(); // 코일 초기화
+	Serial.begin(9600); // Serial 통신속도
+
+}
+
+void loop() {
+  int adc = analogRead(pinIR);	
+	int adc2 = analogRead(pinIR2);
+	double voltage = x * (5.0 / 1023.0);
+
+	// Serial 출력
+	Serial.print("ADC: ");
+	Serial.print(adc);
+	Serial.print("\tVoltage: ");
+	Serial.print(voltage, 2);
+	Serial.print(" V\tDistance: ");
+	Serial.print(mmPrint(adc), 1);
+	Serial.println(" mm");
+
+	rotateDegrees(90.0f);
+	delay(1000);
+	rotateDegrees(-45.0f);
+	delay(1000);
+
+	delay(200);
+}
+
 
 void stepOnce(int dir) {
 	static int idx = 0;
@@ -42,7 +91,7 @@ void stepN(long steps, int dir) {
 	for (long i = 0; i < steps; i++) {
 		stepOnce(dir);
 	}
-	disableCoils(); // 멈출 때 발열 줄이려면 코일 오프
+	disableCoils(); 
 }
 
 void rotateDegrees(float angle_deg) {
@@ -58,49 +107,19 @@ void disableCoils() {
 	digitalWrite(IN4, LOW);
 }
 
-void setup() {
-  pinMode(pinLED, OUTPUT);
-  pinMode(pinBuz, OUTPUT);
-  pinMode(pinIR, INPUT);
-  pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
-
-	disableCoils();
-	
-	
-	Serial.begin(9600);
-
-  digitalWrite(pinLED, HIGH);
+void warning(){
+	digitalWrite(pinLED, HIGH);
   tone(pinBuz, 3000);
-  delay(5000);
   digitalWrite(pinLED, LOW);
   noTone(pinBuz);
-
 }
 
-void loop() {
-  int adc = analogRead(pinIR);		// ADC 0~1023
-	double voltage = adc * (5.0 / 1023.0);
+double mmPrint(int x){
 
-  float distanceCM = 2076.0 / (adc - 11.0);
+  float distanceCM = 2076.0 / (x - 11.0);
 	if (distanceCM < 4) distanceCM = 4;
 	if (distanceCM > 30) distanceCM = 30;
 	float distanceMM = distanceCM * 10.0;
 
-	// Serial 출력
-	Serial.print("ADC: ");
-	Serial.print(adc);
-	Serial.print("\tVoltage: ");
-	Serial.print(voltage, 2);
-	Serial.print(" V\tDistance: ");
-	Serial.print(distanceMM, 1);
-	Serial.println(" mm");
-
-	rotateDegrees(90.0f);
-	delay(1000);
-	rotateDegrees(-45.0f);
-	delay(1000);
-
+	return distanceMM;
 }
